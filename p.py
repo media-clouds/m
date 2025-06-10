a0c5adae4cfd64a937f3b3c2e44a8576f0a2c13d
media-clouds <ditss.tech@gmail.com>
2025-06-10 09:44:23 +0700

    Create p.py

---
+++ p.py
@@ -0,0 +1,13 @@
+/*
+ * -----------------------------------------------------------------------------
+ *  Created by    : ditss
+ *  GitHub        : https://github.com/ditss-dev
+ *  WhatsApp      : https://wa.me/6281513607731
+ *  Saluran       : https://whatsapp.com/channel/0029VaimJO0E50UaXv9Z1J0L
+ *  Description   : Source code project Asuma Gen 2 - WhatsApp Bot
+ *  Created Date  : 2025.
+ * -----------------------------------------------------------------------------
+ *  Feel free to use and modify this script.
+ *  Please keep the header intact when distributing.
+ * -----------------------------------------------------------------------------
+ */
